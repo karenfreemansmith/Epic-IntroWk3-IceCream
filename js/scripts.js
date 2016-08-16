80a6ddd9aa9a627640ae8fe3fc9fdf8c7f5c2bab
Angie Smith and Karen Freeman-Smith <angie+karen@>
2016-08-15 18:07:04 -0700

    added more flavors

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $("h1").click(function() {
-    var flavors=["Vanilla", "Bubble_Gum", "Birthday_Cake", "Black_Raspberry_Crunch", "Black_Walnut", "Computer_Chip", "Industrial_Chocolate", "Puke"];
+    var flavors=[
+      "Vanilla", "Bubble_Gum", "Birthday_Cake", "Black_Raspberry_Crunch", "Black_Walnut", "Computer_Chip", "Industrial_Chocolate", "Honeydew", "Blue_Moon",
+      "Blueberry", "Caramel_Praline", "Chocolate",
+      "Chocolate_Brownies", "Chocolate_Chip", "Chocolate_Custard",
+      "Coffee", "Cookies_&_Dough", "Cotton_Candy",
+      "French_Vanilla", "Fudge_Marble", "Kulfi",
+      "Lavender_Honey", "Lemon_Cookie", "Lychee",
+      "Marshmallow_Treat", "Mexican_Chocolate", "Mint_Chocolate_Chip",
+      "Mint_Cookies", "Mocha_Almond_Fudge", "Peach",
+      "Peppermint_Chip", "Pistachio_Almond", "Rainbow_Sherbert",
+      "Rocky_Road", "Rose", "Saffron_Pistachio",
+      "Salty_Caramel", "Sideways", "Strawberry",
+      "Sweet_Coconut", "Swiss_Mocha", "Tiger_Tails",
+      "Toasted_Almond", "Toffeeness", "Vanilla_Bean",
+      "White_Chocolate_Ginger"
+    ];
     flavors.forEach(function(flavor) {
       var someText = "<div class='col-sm-3'><h3>" + flavor + "</h3><img src='img/"+flavor+".jpg'></div>";
       $("div.row").append(someText);
